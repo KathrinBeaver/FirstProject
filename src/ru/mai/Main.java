d5608207d00c1b907443e424108f219091323a07
KathrinBeaver <kathrin.beaver@mail.ru>
2021-03-20 08:13:34 +0300

    Add examples

--- src/ru/mai/Main.java
+++ src/ru/mai/Main.java
@@ -5,8 +5,16 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
+        typesExample();
+        arraysExample();
+        exceptionsExampleInt();
+        exceptionsExampleFloat();
+    }
 
-        // Arrays and foreach
+    /**
+     * Arrays and foreach example
+     */
+    private static void arraysExample() {
         int[] array = new int[10];
         Arrays.fill(array, 1000);
 
@@ -22,6 +30,42 @@ public class Main {
         for(int el : smallArray) {
             System.out.println(el);
         }
+    }
+
+    private static void typesExample() {
+        char ch = '9';
+        int chInt = Character.getNumericValue(ch);
+        int chInt10 = Character.digit(ch, 10);
+        int chInt8 = Character.digit(ch, 8);
+        System.out.println(ch);
+        System.out.println(chInt);
+        System.out.println(chInt10);
+        System.out.println(chInt8);
+    }
+
+    private static void exceptionsExampleInt() {
+        int a = 10, b = 0, res = 0;
+        try {
+            res = a / b;
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+            System.out.println("Ошибка деления на 0");
+            System.out.println(e.getMessage());
+        }
+        System.out.println(res);
+    }
+
+    private static void exceptionsExampleFloat() {
+        float a = 10, b = 0, res = 0;
+
+        try {
+            res = a / b;
+        } catch (ArithmeticException e) {
+            e.printStackTrace();
+            System.out.println("Ошибка деления на 0");
+            System.out.println(e.getMessage());
+        }
+        System.out.println(res);
     }
 
 }
